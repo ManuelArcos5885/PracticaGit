cd765e0899fbfcaefbbf6ac81e5618038e3c52b3
ManuelArcos5885 <manuarcos10@gmail.com>
2022-12-12 14:54:40 +0100

    Inicio

--- operaciones/Operaciones.java
+++ operaciones/Operaciones.java
@@ -1,10 +1,25 @@
 package operaciones;
 
+import java.util.Scanner;
+
 public class Operaciones {
 
 	public static void main(String[] args) {
 		System.out.println("Menu");
+		Scanner sc = new Scanner (System.in);
+		int numUno,numDos,numTres;
+		
+		System.out.println("Escriba tres numeros: ");
+		System.out.println("Primer numero: ");
+		numUno = sc.nextInt();
+		System.out.println("Segundo numero: ");
+		numDos = sc.nextInt();
+		System.out.println("Tercers numero: ");
+		numTres = sc.nextInt();
+		
+		
 
+	
 	}
 
 }
